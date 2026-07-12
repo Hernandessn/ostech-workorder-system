6905671bfe5fbfe56c8a69aed3708fb84cfd0ef4
hernandessn <hernandessalesneves7@gmail.com>
2026-07-12 10:52:25 -0300

    feat(domain): encapsulate entities and add business rules

--- OSTech.Domain/Entities/Technician.cs
+++ OSTech.Domain/Entities/Technician.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OSTech.Domain.Exceptions;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,13 +7,65 @@ namespace OSTech.Domain.Entities
 {
     public class Technician
     {
-        public int TechnicianId { get; set; }
-        public string Name { get; set; }
-        public string Specialty { get; set; }
-        public string Contact {  get; set; }
-        public bool Availability { get; set; }
-        public DateOnly HiringDate { get; set; }
+        public int TechnicianId { get; private set; }
+        public string Name { get; private set; }
+        public string Specialty { get; private set; }
+        public string Contact {  get; private set; }
+        public bool Availability { get; private set; }
+        public DateOnly HiringDate { get; private set; }
 
-        public ICollection<WorkOrder> WorkOrders { get; set; } = new List<WorkOrder>();
+        public ICollection<WorkOrder> WorkOrders { get; private set; } = new List<WorkOrder>();
+
+        private Technician()
+        {
+        }
+        public Technician
+        (
+            string name,
+            string specialty,
+            string contact,
+            bool availability,
+            DateOnly hiringDate
+        )
+        {
+            SetName(name);
+            SetSpecialty(specialty);
+            SetContact(contact);
+            SetAvailability(availability);
+            SetHiringDate(hiringDate);
+        }
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new DomainException("O nome é obrigatório.");
+
+            Name = name;
+        }
+        public void SetSpecialty(string specialty)
+        {
+            if (string.IsNullOrWhiteSpace(specialty))
+                throw new DomainException("A especialidade é obrigatório.");
+
+            Specialty = specialty;
+        }
+        public void SetContact(string contact)
+        {
+            if (string.IsNullOrWhiteSpace(contact))
+                throw new DomainException("O contato é obrigatório.");
+
+            Contact = contact;
+        }
+        public void SetHiringDate(DateOnly hiringDate)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+
+            if (hiringDate > today)
+                throw new DomainException("A data de contratação não pode ser maior que a de hoje.");
+            HiringDate = hiringDate;
+        }
+        public void SetAvailability(bool availability)
+        {
+            Availability = availability;
+        }
     }
 }
--- OSTech.Domain/Entities/WorkOrder.cs
+++ OSTech.Domain/Entities/WorkOrder.cs
@@ -1,4 +1,5 @@
 ﻿using OSTech.Domain.Entities.Enums;
+using OSTech.Domain.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,16 +8,119 @@ namespace OSTech.Domain.Entities
 {
     public class WorkOrder
     {
-        public int WorkOrderId { get; set; }
-        public string Client {  get; set; }
-        public string Description { get; set; }
-        public string Title { get; set; }
-        public decimal Amount { get; set; }
-        public DateOnly Deadline { get; set; }
-        public DateOnly OpeningDate { get; set; }
-        public StatusWorkOrder Status { get; set; }
-        public int TechnicianId { get; set; } // FK
-        public Technician? Technician { get; set; }
+        public int WorkOrderId { get; private set; }
+        public string Client { get; private set; }
+        public string Description { get; private set; }
+        public string Title { get; private set; }
+        public decimal Amount { get; private set; }
+        public DateOnly Deadline { get; private set; }
+        public DateOnly OpeningDate { get; private set; }
+        public StatusWorkOrder Status { get; private set; }
+        public int TechnicianId { get; private set; } // FK
+        public Technician? Technician { get; private set; }
 
+        private WorkOrder()
+        {
+
+        }
+        public WorkOrder
+        (
+            string client,
+            string description,
+            string title,
+            decimal amount,
+            DateOnly deadline,
+            DateOnly openingDate,
+            int technicianId
+            )
+        {
+            SetClient(client);
+            SetDescription(description);
+            SetTitle(title);
+            SetAmount(amount);
+
+            OpeningDate = openingDate;
+
+            Status = StatusWorkOrder.Open;
+            ChangeDeadline(deadline); 
+            AssignTechnician(technicianId);
+        }
+        public void AssignTechnician(int technicianId)
+        {
+            if (technicianId <= 0)
+            {
+                throw new DomainException("Técnico inválido.");
+            }
+            TechnicianId = technicianId;
+        }
+        public void ChangeDeadline(DateOnly deadline)
+        {
+            if (deadline < OpeningDate)
+            {
+                throw new DomainException("A data de término não pode ser menor que a data de abertura.");
+            }
+            Deadline = deadline;
+        }
+        public void SetDescription(string description)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new DomainException("Descrição inválida.");
+            }
+            Description = description;
+        }
+        public void SetClient(string client)
+        {
+            if (string.IsNullOrWhiteSpace(client))
+            {
+                throw new DomainException("Cliente inválido.");
+            }
+            Client = client;
+        }
+        public void SetTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new DomainException("Título inválido.");
+            }
+            Title = title;
+        }
+        public void SetAmount(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new DomainException("O valor deve ser maior que zero.");
+            }
+            Amount = amount;
+        }
+        public void Start()
+        {
+            if (Status != StatusWorkOrder.Open)
+                throw new DomainException(
+                    "Somente ordens abertas podem ser iniciadas.");
+
+            Status = StatusWorkOrder.InProgress;
+        }
+        public void Complete()
+        {
+            if (Status != StatusWorkOrder.InProgress)
+                throw new DomainException(
+                    "Somente ordens em andamento podem ser concluídas.");
+
+            Status = StatusWorkOrder.Completed;
+        }
+
+        public void Cancel()
+        {
+            if (Status == StatusWorkOrder.Completed)
+                throw new DomainException(
+                    "Uma ordem concluída não pode ser cancelada.");
+
+            if (Status == StatusWorkOrder.Canceled)
+                throw new DomainException(
+                    "A ordem já está cancelada.");
+
+            Status = StatusWorkOrder.Canceled;
+        }
     }
 }
